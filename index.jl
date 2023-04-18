1f495d33f174d7382c168c11961f7b98826c9e9a
lucasmsoares96 <lucasmsoares96@gmail.com>
2023-04-18 14:13:03 +0000

    Deploying to gh-pages from @ lucasmsoares96/Controle-Digital-de-Sistemas-Dinamicos@3ef85aaf4522437c701b04a0e1d2ffaf38f6f6a3 🚀

---
+++ index.jl
@@ -0,0 +1,25 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 21fe60ce-ddf2-11ed-125c-9bbd693774a9
+md"""
+# Controle Digital de Sistemas Dinâmicos
+"""
+
+# ╔═╡ 63f4a031-4cfb-4eb2-a1fe-74743d893a11
+md"""
+1. [Sinais](https://lucasmsoares96.github.io/Controle-Digital-de-Sistemas-Dinamicos/Sinais.html)
+1. [Potência do Sinal](https://lucasmsoares96.github.io/Controle-Digital-de-Sistemas-Dinamicos/Pot%C3%AAncia%20do%20Sinal.html)
+1. [Energia Sinal](https://lucasmsoares96.github.io/Controle-Digital-de-Sistemas-Dinamicos/Energia%20Sinal.html)
+1. [Convolução](https://lucasmsoares96.github.io/Controle-Digital-de-Sistemas-Dinamicos/Convolu%C3%A7%C3%A3o.html)
+1. [Transformada Z](https://lucasmsoares96.github.io/Controle-Digital-de-Sistemas-Dinamicos/Transformada%20Z.html)
+1. [Polos e Zeros(LTI)](https://lucasmsoares96.github.io/Controle-Digital-de-Sistemas-Dinamicos/Polos&Zeros(LTI).html)
+1. [Relação Sinal Continuo e Discreto](https://lucasmsoares96.github.io/Controle-Digital-de-Sistemas-Dinamicos/Rela%C3%A7%C3%A3o%20Sinal%20Continuo%20e%20Discreto.html)
+"""
+
+# ╔═╡ Cell order:
+# ╟─21fe60ce-ddf2-11ed-125c-9bbd693774a9
+# ╟─63f4a031-4cfb-4eb2-a1fe-74743d893a11
